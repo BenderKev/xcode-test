2d005cacd0c2417ae24472a269193b79da6abdf4
schemery <schemery@users.noreply.github.com>
2022-01-11 11:58:22 -0500

    Initial Commit

---
+++ Shared/xcode_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_testApp.swift
+//  Shared
+//
+//  Created by Kevin Schemery on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct xcode_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
